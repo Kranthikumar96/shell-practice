819979184bc0e4e40c684555cbb4a4e5793d12ae
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-05 00:52:17 -0700

    datatype

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -6,6 +6,7 @@ echo "Enter second number to ADD"
 read NUMBER2
 
 TIMESTAMP=$(date)
+echo "script executed at : $TIMESTAMP"
 
-SUM=(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 echo "sum of two number $NUMBER1 & $NUMBER2 is $SUM"
