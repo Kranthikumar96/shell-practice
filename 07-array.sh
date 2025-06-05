2d0776ddc5b0295b86e2eb6d878c028796c43f6c
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-05 00:54:57 -0700

    arrays

--- 07-array.sh
+++ 07-array.sh
@@ -4,7 +4,7 @@ MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
 
 echo "FIRST movie is ${MOVIES[0]}"
 echo "SECOND movie is ${MOVIES[1]}"
-echo "THIRD movie is ${MOVIES [2]}"
+echo "THIRD movie is ${MOVIES[2]}"
 echo "FOURTH movie is ${MOVIES[3]}"
 
 echo "ALL movies are ${MOVIES[@]}"
