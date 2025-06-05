d492d0d5e203a517ee5d9188cb12014d1636f9b7
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-05 00:39:39 -0700

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,8 +3,8 @@
 PEROSN1=Trump
 PERSON2=Musk
 
-echo "$PEROSN1 :: Hey PEROSN2, How are you ?"
-echo "$PEROSN2 :: Hello PEROSN1, I am fine, How are yopu doing"
+echo "$PEROSN1 :: Hey $PEROSN2, How are you ?"
+echo "$PEROSN2 :: Hello $PEROSN1, I am fine, How are yopu doing"
 echo "$PEROSN1 :: I am fine too, What's up ?"
 echo "$PEROSN2 :: Nothing, Just going to Moon, for fun, Will you join ?"
 echo "$PEROSN1 :: Sorry, You carry on! I will come once you come back"
