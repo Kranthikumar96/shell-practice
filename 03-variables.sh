4b52597d7e0d750d59dfcbea1d38a7ac889f1e10
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-04 23:24:12 -0700

    adding variables file

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "person1 :: Hey PEROSN2, How are you ?"
+echo "PEROSN2 :: Hello person1, I am fine, How are yopu doing"
+echo "person1 :: I am fine too, What's up ?"
+echo "PEROSN2 :: Nothing, Just going to Moon, for fun, Will you join ?"
+echo "person1 :: Sorry, You carry on! I will come once you come back"
