20012c04ec9a2a9769d4175dfca3228b84df8e99
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-06-05 00:04:33 -0700

    adding data types file

---
+++ 06-datatypes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter first number to ADD"
+read NUMBER1
+echo "Enter second number to ADD"
+read NUMBER2
+
+TIMESTAMP=$(date)
+
+SUM=(($NUMBER1+$NUMBER2))
+echo "sum of two number $NUMBER1 & $NUMBER2 is $SUM"
